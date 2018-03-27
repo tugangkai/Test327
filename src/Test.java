960ab52541efb74fc3400b102a07de607d5b5999
tugangkai <tugangkai@126.com>
2018-03-28 01:36:30 +0800

    修改代码

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,24 @@
+import java.util.LinkedList;
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        System.out.println("hello world");
+        LinkedList list =new LinkedList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+
+        //zhushi
+
+        list.remove(3);
+
+
+        for(Object o:list){
+            System.out.println((Integer)o);
+        }
+
     }
 }
