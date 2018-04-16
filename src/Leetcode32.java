2b30963b162d64bd7e8b3367a2bc37b2ac238bb4
tugangkai <tugangkai@126.com>
2018-04-16 21:43:48 +0800

    leetcode32增加动态规划方法

--- src/Leetcode32.java
+++ src/Leetcode32.java
@@ -13,13 +13,8 @@ public class Leetcode32 {
                     list.pop();
                     continue;
                 }
-
-
             }
             list.push(i);
-
-
-
         }
         int max=0;
         int cur=0;
@@ -37,18 +32,42 @@ public class Leetcode32 {
         System.out.println(max);
         return Math.max(nextcur,max);
 
+    }
+
+    /**
+     *
+     * 动态规划解法
+     * @param s
+     * @return
+     */
+    public int getLongestBracket(String s){
+        int len=s.length();
+        int dp[]=new int[len];
+        int max=0;
+        for(int i=0;i<len;i++){
+            char c=s.charAt(i);
+          if(c==')'&&i-dp[i-1]-1>=0&&s.charAt(i-dp[i-1]-1)=='('){
+              dp[i]=dp[i-1]+2+(i-dp[i-1]-2>=0?dp[i-dp[i-1]-2]:0);
+              max=Math.max(dp[i],max);
+
+          }
 
 
+        }
 
-
-
+        return max;
 
 
     }
+
+
+
 
     public static void main(String[] args) {
         Leetcode32 l32=new Leetcode32();
         int s= l32.longestValidParentheses("()(()");
         System.out.println(s);
+        int t=l32.getLongestBracket("()(()");
+        System.out.println(t);
     }
 }
