cd341b37c59673f059c156179228276f3ad7db9b
tugangkai <tugangkai@126.com>
2018-05-29 01:18:49 +0800

    一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
    机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
    
    现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？
    
    网格中的障碍物和空位置分别用 1 和 0 来表示。
    
    说明：m 和 n 的值均不超过 100。
    
    示例 1:
    
    输入:
    [
      [0,0,0],
      [0,1,0],
      [0,0,0]
    ]
    输出: 2
    解释:
    3x3 网格的正中间有一个障碍物。
    从左上角到右下角一共有 2 条不同的路径：
    1. 向右 -> 向右 -> 向下 -> 向下
    2. 向下 -> 向下 -> 向右 -> 向右

---
+++ src/Leetcode63.java
@@ -0,0 +1,100 @@
+public class Leetcode63 {
+
+//    一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+//
+//    机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+//
+//    现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？
+//
+//
+//
+//    网格中的障碍物和空位置分别用 1 和 0 来表示。
+//
+//    说明：m 和 n 的值均不超过 100。
+//
+//    示例 1:
+//
+//    输入:
+//            [
+//            [0,0,0],
+//            [0,1,0],
+//            [0,0,0]
+//            ]
+//    输出: 2
+//    解释:
+//            3x3 网格的正中间有一个障碍物。
+//    从左上角到右下角一共有 2 条不同的路径：
+//            1. 向右 -> 向右 -> 向下 -> 向下
+//2. 向下 -> 向下 -> 向右 -> 向右
+//
+//
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        //高度
+        int m=obstacleGrid.length;
+        //宽度
+        int n=obstacleGrid[0].length;
+        int [][] res=new int[m][n];
+        boolean f1=true;
+        for(int i=0;i<m;i++)
+        {
+            if(obstacleGrid[i][0]==1&&f1){
+                res[i][0]=0;
+                f1=false;
+            }
+            if(!f1){
+                res[i][0]=0;
+            }else{
+                res[i][0]=1;
+            }
+        }
+        boolean f2=true;
+        for(int j=0;j<n;j++){
+            if(obstacleGrid[0][j]==1&&f2){
+                res[0][j]=0;
+                f2=false;
+            }
+            if(!f2){
+                res[0][j]=0;
+
+            }else{
+                res[0][j]=1;
+            }
+        }
+
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+
+                if(obstacleGrid[i][j]==1){
+                    res[i][j]=0;
+                }else{
+                    res[i][j]=res[i-1][j]+res[i][j-1];
+                }
+
+
+
+            }
+
+
+        }
+
+
+        return res[m-1][n-1];
+
+
+    }
+
+
+    public static void main(String[] args) {
+        Leetcode63 l63=new Leetcode63();
+        int [][]grid=new int [][]{
+                {0,0,0},
+                {0,1,0},
+                {0,0,0}
+
+
+        };
+        int res=l63.uniquePathsWithObstacles(grid);
+        System.out.println(res);
+
+    }
+}
