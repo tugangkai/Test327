306c024122f9a68738ed5adf08ac79d1893bf933
tugangkai <tugangkai@126.com>
2018-06-12 22:36:16 +0800

    完全二叉树链接节点

---
+++ src/Leetcode116.java
@@ -0,0 +1,30 @@
+class TreeLinkNode{
+    TreeLinkNode left;
+    TreeLinkNode right;
+    TreeLinkNode next;
+    int val;
+    public TreeLinkNode(int val){
+        this.val=val;
+    }
+}
+
+public class Leetcode116 {
+
+
+    public void connect(TreeLinkNode root) {
+        TreeLinkNode level_start=root;
+        while(level_start!=null){
+            TreeLinkNode cur=level_start;
+            while(cur!=null){
+                if(cur.left!=null)
+                    cur.left.next=cur.right;
+                if(cur.right!=null&&cur.next!=null)
+                    cur.right.next=cur.next.left;
+                cur=cur.next;
+            }
+            level_start=level_start.left;
+        }
+    }
+
+
+}
