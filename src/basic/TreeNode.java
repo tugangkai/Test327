91263db2245e73a61b51cebc96bd415cb435a1ab
tugangkai <tugangkai@126.com>
2018-07-16 23:39:37 +0800

    树节点的定义

---
+++ src/basic/TreeNode.java
@@ -0,0 +1,15 @@
+package basic;
+
+public class TreeNode {
+    public  TreeNode left;
+    public TreeNode right;
+    public  int val;
+    public TreeNode(int val){
+        this.val=val;
+    }
+    public TreeNode(){
+
+    }
+
+
+}
